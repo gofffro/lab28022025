b022e8c2e85c084eef6a1742969cc7bc094739cb
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-02-26 22:56:19 +0700

    Добавлен класс-наследник WordDocument и переопределен метод

--- Document.cs
+++ Document.cs
@@ -24,7 +24,28 @@ namespace DocumentApp
         Console.Write($"{word}\t");
       }
       Console.WriteLine($"Тема документа: {Theme}");
-      Console.WriteLine($"Путь документа: {PathDoc}");                              
+      Console.WriteLine($"Путь документа: {PathDoc}");
+    }
+
+    public class WordDocument : Document
+    {
+      public int WordCount { get; set; }
+
+      public WordDocument(string name, string author, List<string> keywords, string theme, string pathDoc, int wordCount)
+      {
+        Name = name;
+        Author = author;
+        Keywords = keywords;
+        Theme = theme;
+        PathDoc = pathDoc;
+        WordCount = wordCount;
+      }
+
+      public override void DisplayInfo()
+      {
+        base.DisplayInfo();
+        Console.WriteLine($"Количество слов: {WordCount}");
+      }
     }
   }
 }
