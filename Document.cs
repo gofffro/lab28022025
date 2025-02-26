873689dc26a5bb614544e3840d8cebac21b56c0d
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-02-26 22:12:35 +0700

    Добавлены свойства, реализован виртуальный метод

--- Document.cs
+++ Document.cs
@@ -8,5 +8,22 @@ namespace Document
 {
   public class Document
   {
+    public string Name { get; set; }
+    public string Author { get; set; }
+    public List<string> Keywords { get; set; } = new List<string>();
+    public string Theme { get; set; }
+    public string PathDoc { get; set; }
+
+    public virtual void DisplayInfo()
+    {
+      Console.WriteLine($"Имя документа: {Name}");
+      Console.WriteLine($"Автор документа: {Author}");
+      Console.WriteLine($"Ключевые слова: ");
+      foreach (var word in Keywords)
+      {
+        Console.Write($"{word}\t");
+      }
+      Console.WriteLine($"Путь документа: {PathDoc}");                              
+    }
   }
 }
