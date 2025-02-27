8f43cb226fa217652aed234a242497cd2a6aa278
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-02-27 21:44:32 +0700

    Добавлено меню

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace DocumentApp
+﻿
+namespace DocumentApp
 {
   internal class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Test commit");
+      SingletonDocument docManager = SingletonDocument.GetInstance();
+      ShowMenu(docManager);
+    }
+    static void ShowMenu(SingletonDocument docManager)
+    {
+      string userInput;
+      do
+      {
+        Console.WriteLine("Меню: ");
+        Console.WriteLine("1. Добавить документ");
+        Console.WriteLine("2. Список документов");
+        Console.WriteLine("3. Выбрать документ");
+        Console.WriteLine("4. Выйти");
+        userInput = Console.ReadLine();
+      } while (userInput != "4");
     }
   }
 }
