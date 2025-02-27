a68a941e90c7fd4d7ffd8f311b5d46698f1cf542
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-02-27 22:27:28 +0700

    Реализован метод AddDoc()

--- Program.cs
+++ Program.cs
@@ -40,5 +40,57 @@ namespace DocumentApp
         }
       } while (userInput != "4");
     }
+
+    static void AddDoc(SingletonDocument docManager)
+    {
+      Console.Write("Введите имя документа: ");
+      string name = Console.ReadLine();
+
+      Console.Write("Введите автора документа: ");
+      string author = Console.ReadLine();
+
+      Console.Write("Введите ключевые слова через запятую например: (классика, драма): ");
+      List<string> keywords = new List<string>(Console.ReadLine().Split(','));
+
+      Console.Write("Введите тему документа: ");
+      string theme = Console.ReadLine();
+
+      Console.Write("Введите путь к документу: ");
+      string pathDoc = Console.ReadLine();
+
+      Console.Write("Выберите тип документа ([1]Word, [2]PDF, [3]Excel, [4]TXT, [5]HTML): ");
+      string docType = Console.ReadLine();
+
+      Document doc = null;
+
+      switch (docType)
+      {
+        case "1":
+          doc = new WordDocument(name, author, keywords, theme, pathDoc, GetWordCount());
+          break;
+        case "2":
+          doc = new PdfDocument(name, author, keywords, theme, pathDoc, GetPageCount());
+          break;
+        case "3":
+          doc = new ExcelDocument(name, author, keywords, theme, pathDoc, GetSheetCount());
+          break;
+        case "4":
+          doc = new TxtDocument(name, author, keywords, theme, pathDoc, GetSymbolCount());
+          break;
+        case "5":
+          doc = new HtmlDocument(name, author, keywords, theme, pathDoc, GetTagCount());
+          break;
+        default:
+          Console.WriteLine("Ошибка!");
+          return;
+      }
+
+      if (doc != null)
+      {
+        docManager.AddDocument(doc);
+        Console.WriteLine("Документ добавлен.");
+      }
+
+    }
   }
 }
